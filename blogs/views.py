1a37cd1372a5b81a0d9e4c83e00b654388291ffb
KutuMia <sohanurhimel@gmail.com>
2026-06-29 00:16:48 +0600

    post by category

--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render, get_object_or_404
+
+from .models import Blog, Category
 
 # Create your views here.
+def post_by_category(request, category_id):
+    posts = Blog.objects.filter(category_id=category_id, status='Published').order_by('updated_at')
+    # try:
+    #     category = Category.objects.get(pk=category_id)
+    # except:
+    #     return redirect('home')  # Redirect to home if category does not exist
+
+    category = get_object_or_404(Category, pk=category_id)
+
+
+    context = {
+        "posts": posts,
+        "category": category,
+    }
+    return render(request, 'post_by_category.html', context)
